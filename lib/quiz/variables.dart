35d24767c050ba109ee0e5cdff4d485745b7e0e7
arafatomer <arafatomer66@gmail.com>
2020-11-12 18:11:04 +0600

    Row Colum

---
+++ lib/quiz/variables.dart
@@ -0,0 +1,32 @@
+int _questionIndex = 0;
+int _totalScore = 0;
+
+const questions = [
+  {
+    'questionText': 'What\'s your favourite color',
+    'answer': [
+      {'text': 'red', 'score': 10},
+      {'text': 'black', 'score': 12},
+      {'text': 'white', 'score': 5},
+      {'text': 'green', 'score': 7},
+    ],
+  },
+  {
+    'questionText': 'What\'s your favourite animal',
+    'answer': [
+      {'text': 'dog', 'score': 10},
+      {'text': 'cat', 'score': 12},
+      {'text': 'pig', 'score': 5},
+      {'text': 'goat', 'score': 7},
+    ],
+  },
+  {
+    'questionText': 'What\'s your favourite food',
+    'answer': [
+      {'text': 'pizza', 'score': 10},
+      {'text': 'pasta', 'score': 12},
+      {'text': 'burger', 'score': 5},
+      {'text': 'chicken', 'score': 7},
+    ],
+  },
+];
